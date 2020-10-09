1d171a27812704312af0611f23267f8999e449ce
unknown <hafidardiansyahh@gmail.com>
2020-10-09 09:25:33 +0700

    add intro screen

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,21 +1,50 @@
-import 'dart:ui';
-
-import 'package:absensi_sekolah/components/back.dart';
-import 'package:absensi_sekolah/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:absensi_sekolah/utilities/constants.dart';
 
 class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          child: Back(),
-        )
-      ],
-    ));
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Center(
+          child: Text(
+            "Absensi Siswa",
+            style: TextStyle(
+              fontFamily: "Poppins-Bold",
+              color: primaryColor,
+              fontSize: 32,
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: size.height * 0.1,
+          ),
+          Center(
+            child: Text(
+              "Absensi Siswa",
+              style: TextStyle(
+                fontFamily: "Poppins-Bold",
+                color: primaryColor,
+                fontSize: 32,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: size.height * 0.1,
+          ),
+          SvgPicture.asset(
+            "assets/images/intro.svg",
+            width: size.width * 0.9,
+          ),
+        ],
+      ),
+    );
   }
 }
