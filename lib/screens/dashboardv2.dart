a756a53002e8e046508d31b09b23ea49c7b30cd8
unknown <hafidardiansyahh@gmail.com>
2020-10-19 09:05:56 +0700

    profile button done

--- lib/screens/dashboardv2.dart
+++ lib/screens/dashboardv2.dart
@@ -1,3 +1,4 @@
+import 'package:absensi_sekolah/components/rounded_profile_button_v2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:absensi_sekolah/utilities/constants.dart';
@@ -24,104 +25,13 @@ class _Dashboardv2State extends State<Dashboardv2> {
               top: size.height * 0.18,
               left: -2,
               child: SvgPicture.asset("assets/images/house.svg")),
-          RoundedProfileButton(),
+          RoundedProfileButtonV2(
+              image: AssetImage("assets/images/ig.png"),
+              name: "Hafid",
+              status: "Status",
+              onTap: () {}),
           SelectionContainer()
         ]));
-  }
-}
-
-class RoundedProfileButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-
-    return LayoutBuilder(builder: (context, contraints) {
-      if (contraints.maxHeight > 600) {
-        return Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            width: size.width * 0.9,
-            height: size.height * 0.07,
-            margin: EdgeInsets.symmetric(
-              vertical: size.height * 0.06,
-              horizontal: size.width * 0.04,
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            decoration: BoxDecoration(
-                color: greyColor, borderRadius: BorderRadius.circular(30)),
-            child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-              CircleAvatar(
-                  backgroundImage: AssetImage("assets/images/profile.png")),
-              SizedBox(width: size.width * 0.02),
-              GestureDetector(
-                onTap: () {},
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Hi, Hafid",
-                        style: TextStyle(
-                          color: blackColor,
-                          fontFamily: "Poppins-Medium",
-                          fontSize: 18,
-                        )),
-                    Text("Siswa, XII RPL 3",
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          color: grey2Color,
-                          fontFamily: "Poppins-Regular",
-                          fontSize: 12,
-                        )),
-                  ],
-                ),
-              ),
-            ]),
-          ),
-        );
-      } else {
-        return Align(
-          alignment: Alignment.topCenter,
-          child: Container(
-            width: size.width * 0.9,
-            height: size.height * 0.06,
-            margin: EdgeInsets.symmetric(
-              vertical: size.height * 0.04,
-              horizontal: size.width * 0.04,
-            ),
-            padding: EdgeInsets.symmetric(horizontal: 6),
-            decoration: BoxDecoration(
-                color: greyColor, borderRadius: BorderRadius.circular(30)),
-            child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-              CircleAvatar(
-                  backgroundImage: AssetImage("assets/images/profile.png")),
-              SizedBox(width: size.width * 0.02),
-              GestureDetector(
-                onTap: () {},
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Hi, Hafid",
-                        style: TextStyle(
-                          color: blackColor,
-                          fontFamily: "Poppins-Medium",
-                          fontSize: 16,
-                        )),
-                    Text("Siswa, XII RPL 3",
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(
-                          color: grey2Color,
-                          fontFamily: "Poppins-Regular",
-                          fontSize: 10,
-                        )),
-                  ],
-                ),
-              ),
-            ]),
-          ),
-        );
-      }
-    });
   }
 }
 
@@ -156,32 +66,112 @@ class SelectionContainer extends StatelessWidget {
                         fontFamily: "Poppins-Medium",
                       ),
                     ),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        width: size.width * 0.25,
-                        height: size.height * 0.05,
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        decoration: BoxDecoration(
-                            color: superLightColor,
-                            borderRadius: BorderRadius.circular(30)),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Icon(Icons.filter_list, color: primaryColor),
-                            Text(
-                              "Filter",
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: blackColor,
-                                fontFamily: "Poppins-Medium",
+                    Material(
+                      color: superLightColor,
+                      borderRadius: BorderRadius.circular(30),
+                      child: InkWell(
+                        onTap: () {},
+                        splashColor: primaryColor,
+                        borderRadius: BorderRadius.circular(30),
+                        child: Container(
+                          width: size.width * 0.25,
+                          height: size.height * 0.05,
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Icon(Icons.filter_list, color: primaryColor),
+                              Text(
+                                "Filter",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: blackColor,
+                                  fontFamily: "Poppins-Medium",
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ],
+                ),
+                Container(
+                  width: size.width * 0.9,
+                  height: size.height * 0.07,
+                  margin: EdgeInsets.symmetric(
+                    vertical: size.height * 0.02,
+                    horizontal: size.width * 0.01,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  decoration: BoxDecoration(
+                      color: greyColor,
+                      borderRadius: BorderRadius.circular(30)),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            ClipOval(
+                                child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Icon(
+                                Icons.file_present,
+                                color: primaryColor,
+                                size: 32,
+                              ),
+                            )),
+                            Material(
+                              child: Flexible(
+                                child: Container(
+                                  width: size.width * 0.55,
+                                  color: greyColor,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text("Pengumuman rapot",
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                            color: blackColor,
+                                            fontFamily: "Poppins-Medium",
+                                            fontSize: 16,
+                                          )),
+                                      Text("19 Okt 2020",
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                            color: grey2Color,
+                                            fontFamily: "Poppins-Regular",
+                                            fontSize: 10,
+                                          )),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        ClipOval(
+                            child: Material(
+                          color: primaryColor, // button color
+                          child: InkWell(
+                            splashColor: darkColor, // inkwell color
+                            child: SizedBox(
+                              width: 50,
+                              height: 50,
+                              child: Icon(
+                                Icons.navigate_next,
+                                color: whiteColor,
+                                size: 32,
+                              ),
+                            ),
+                            onTap: () {},
+                          ),
+                        )),
+                      ]),
                 ),
               ])));
         } else {
